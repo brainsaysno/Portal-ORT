ba505bac01f496582b068a5096dedcb56020f8be
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2023-02-07 18:31:02 -0300

    fix: unescaped quote

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({
       {children}
     </main>
     <footer className="bg-[#110000] py-5 text-center text-white">
-      <p className="italic">"Me calenté con gestión y acá estamos"</p>
+      <p className="italic">&quot;Me calenté con gestión y acá estamos&quot;</p>
       <p>&copy; Nicolás Russo</p>
     </footer>
   </div>
