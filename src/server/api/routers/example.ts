6731f277b97cce678d614dd620b96e76e7ae8bb6
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2023-02-07 00:21:15 -0300

    feat: pre db naming refactor

---
+++ src/server/api/routers/example.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const materiaRouter = createTRPCRouter({
+  getById: publicProcedure
+    .input(z.object({ id: z.number().positive().int() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.materia.findFirst({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.materia.findMany();
+  }),
+  getPreviasById: publicProcedure
+    .input(z.object({ id: z.number().positive().int() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.materia.findFirst({
+        where: { id: input.id },
+        include: {
+          previas: {
+            include: {
+              materia_previa_de: true,
+            },
+          },
+          previa_de: {
+            include: {
+              materia_previa: true,
+            },
+          },
+        },
+      });
+    }),
+});
