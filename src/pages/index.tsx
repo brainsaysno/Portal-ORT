a14c52acf1fbc1c84f18eb4468016454ad345a3e
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2023-02-08 18:02:01 -0300

    fix: title size on mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,12 @@ const Home: NextPage = () => {
       <Layout noHeader>
         <div className="flex flex-col items-center ">
           <div className="py-10">
-            <h1 className="text-8xl font-bold text-white">Gestión ORT</h1>
-            <h2 className="text-4xl font-bold text-white">(como la gente)</h2>
+            <h1 className="text-5xl font-bold text-white md:text-8xl">
+              Gestión ORT
+            </h1>
+            <h2 className="text-3xl font-bold text-white md:text-4xl">
+              (como la gente)
+            </h2>
           </div>
           <CustomButton href="/materias">Materias</CustomButton>
         </div>
