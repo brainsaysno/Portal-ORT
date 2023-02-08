93e79a63131f1f238967767d0c8a8e89f31c6482
Nicolás Russo <nicolas.russo.ortiz@gmail.com>
2023-02-07 22:24:19 -0300

    feat: add search bar

--- src/pages/materias/index.tsx
+++ src/pages/materias/index.tsx
@@ -5,9 +5,18 @@ import CustomButton from "../../components/CustomButton";
 
 import { api } from "../../utils/api";
 import Spinner from "../../components/Spinner";
+import { useState } from "react";
+
+const normalizeString = (str: string): string =>
+  str
+    .toUpperCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
 
 const Materias: NextPage = () => {
   const materia = api.materia.getAll.useQuery();
+
+  const [filterPattern, setFilterPattern] = useState("");
 
   return (
     <>
@@ -16,21 +25,35 @@ const Materias: NextPage = () => {
         <meta name="description" content="Materias y previas" />
       </Head>
       <Layout>
-        <div className="flex flex-col items-center ">
-          <h1 className="py-5 text-3xl font-semibold text-white">Materias</h1>
+        <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center gap-5 py-5">
+            <h1 className=" text-3xl font-semibold text-white">Materias</h1>
+            {materia.isSuccess && (
+              <input
+                className="px-4 py-2"
+                onChange={(e) => setFilterPattern(e.target.value)}
+                value={filterPattern}
+                placeholder="Buscar"
+              />
+            )}
+          </div>
           <div className="flex flex-row flex-wrap justify-center gap-2">
             {materia.isLoading && <Spinner className="h-12 w-12" />}
 
-            {materia.data?.map(({ id, nombre }) => (
-              <CustomButton
-                href={`/materias/${id}`}
-                leftText={id}
-                key={id}
-                hover
-              >
-                {nombre}
-              </CustomButton>
-            ))}
+            {materia.data
+              ?.filter(({ nombre }) =>
+                normalizeString(nombre).includes(normalizeString(filterPattern))
+              )
+              .map(({ id, nombre }) => (
+                <CustomButton
+                  href={`/materias/${id}`}
+                  leftText={id}
+                  key={id}
+                  hover
+                >
+                  {nombre}
+                </CustomButton>
+              ))}
           </div>
         </div>
       </Layout>
